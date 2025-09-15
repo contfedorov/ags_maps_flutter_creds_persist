96531de2fc997443b7d4987cff7bcf045e84c814
Egor A. Fedorov <efedorov@dataeast.com>
2025-09-15 17:44:32 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ags_maps_flutter_creds_persist
+
+ArcGIS Maps SDK for Flutter credentials persistence issue
---
+++ android/app/src/main/kotlin/com/example/ags_maps_flutter_creds_persist/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ags_maps_flutter_creds_persist
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
